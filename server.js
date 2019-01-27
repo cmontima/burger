2518aebd6beb444f7ef93a4973df2373e162a93a
Carlos <carlos@montima.mx>
2019-01-27 12:11:00 -0600

    updates

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
+require('dotenv').config();
+
+
+// Bring the express package into this file and put it inside a variable named express.
 var express = require("express");
 
+// Bring in the models.
+var models = require("./models");
+
+// Create a new express server instance and put it inside a variable named app.
 var app = express();
-var PORT = 3000;
 
+// The port for our express server.
+var PORT = 8080;
 
+require('./config/config.js')(app);
+
+models.sequelize.sync().then(function () {
+    console.log("\nDatabase ready \n");
+});
+
+app.listen(PORT, function () {
+    console.log("Server is listening on port", PORT);
+});
